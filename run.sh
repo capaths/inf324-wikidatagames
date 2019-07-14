305c3fd82685e12258cae7582b99198365778aba
pathdll <pcampaa@gmail.com>
2019-07-14 18:55:10 -0400

    Added log and rabbitmq container

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 function clean() {
-    echo "Stopping and removing container $1"
+    echo "[*] Stopping and removing container $1"
     docker rm -f $1
 }
 
 function run() {
-    echo "Running container $1 at port $3"
+    echo "[*] Running container $1 at port $3"
     docker run -p "$3:5000" -d --name $2 $1
 }
 
@@ -18,7 +18,11 @@ Ports=("$access" "$chat" "$match" "$player" "$ticket")
 for i in "${!Services[@]}"; do 
     service="${Services[$i]}"
     port="${Ports[$i]}"
-    
+
     clean "$service-service"
     run "capaths/games-$service" "$service-service"  "${Ports[$i]}"
 done
+
+echo "[*] Running RabbitMQ container"
+docker rm -f games-rabbit
+docker run -d -p 15672:15672 --hostname games-broker --name games-rabbit rabbitmq:3.7.16-management
